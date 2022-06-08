5a5b68933a5f06b5797896ae377e1600358d7d80
Superb <staret.vision@gmail.com>
2022-06-08 20:13:55 +0900

    Initial commit

---
+++ KISClientConsole/KISClientConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace KISClientConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            KISClient client = new KISClient("your appkey"
+                , "your appsecret");
+
+            client.GetHashKey("your accunt number");
+            client.GetAccessToken();
+            //client.RevokeToken();
+
+            client.GetStockPrice("000660");
+        }
+    }
+}
