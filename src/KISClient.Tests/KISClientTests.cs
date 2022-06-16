5f3c9f01847b34531e8be0ba210fa6e82a165d23
Superb <staret.vision@gmail.com>
2022-06-16 22:11:44 +0900

    Make Test Class

---
+++ src/KISClient.Tests/KISClientTests.cs
@@ -0,0 +1,24 @@
+﻿using KISClient.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace KISClient.Tests
+{
+    public class KISClientTests
+    {
+        KISHttpClient client;
+
+        public KISClientTests()
+        {
+
+        }
+
+        [Fact]
+        public void GetHashkey()
+        {
+
+        }
+    }
+}
