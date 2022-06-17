8c83b1a18f42d345288c6d4fbb56de6989ed5e63
Superb <staret.vision@gmail.com>
2022-06-17 17:54:52 +0900

    Add JsonProperty

--- src/KISClient.Core/Model/AccessTokenModel.cs
+++ src/KISClient.Core/Model/AccessTokenModel.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace KISClient.Core.Model
 {
     public class AccessTokenModel : BaseModel
     {
+        [JsonProperty("access_token")]
         public string AccessToken { get; set; }
+
+        [JsonProperty("token_type")]
         public string TokenType { get; set; }
-        public string ExpireSec { get; set; }
     }
 }
