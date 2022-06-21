cc6004456346b00ab5fd2afc4e2c55584b528bc7
Superb <staret.vision@gmail.com>
2022-06-21 16:53:09 +0900

    Add ResponseRESTModel class

---
+++ src/KISClient.Core/Model/ResponseRESTModel.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KISClient.Core.Model
+{
+    public class ResponseRESTModel : BaseModel
+    {
+        /// <summary>
+        /// 0: Success
+        /// other: Failure
+        /// </summary>
+        [JsonProperty("rt_cd")]
+        public string IsSuccessful { get; set; }
+
+        [JsonProperty("msg_cd")]
+        public string MessageCode { get; set; }
+
+        [JsonProperty("msg1")]
+        public string Message { get; set; }
+
+        //output
+    }
+}
